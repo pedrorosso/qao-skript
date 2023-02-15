2786b579d04f7d75dba74b45b1c0eb57bbee01ee
Pedro Rosso Gomez <rpedro@student.ethz.ch>
2023-02-16 00:01:27 +0100

    Main script to generate all

---
+++ main.py
@@ -0,0 +1,6 @@
+from wigner_funcs import generate_wigner_husimi
+from slowness_surf import plot_all_slowness_surfs
+
+if __name__ == "__main__":
+    generate_wigner_husimi()
+    plot_all_slowness_surfs()
